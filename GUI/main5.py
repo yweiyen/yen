7adf4ba84c085f9a9e26fd2ab11df8d798f45c3c
yweiyen <125293819+yweiyen@users.noreply.github.com>
2024-02-03 07:26:42 +0000

    modify

---
+++ GUI/main5.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import requests
+from streamlit_autorefresh import st_autorefresh
+
+st_autorefresh(interval=5000)
+
+st.title("Pico_W_職能發展協會專案")     
+st.header("雞舍:red[溫度]和:blue[光線]狀態")
+st.divider()
+
+url = 'https://blynk.cloud/external/api/get?token=5Kcn_2ZsT0ViHAuKl5DrNhbsmA7nIEkK&v0&v1'
+
+response = requests.request("GET",url)
+if response.status_code == 200:
+    all_data = response.json()
+    st.info(f'光線:{all_data["v0"]}')
+    st.warning(f'可變電阻:{all_data["v1"]}')
+else:
+    st.write("連線失敗,請等一下再試")
+    
