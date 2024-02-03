bd53a8c005e7c362a8b2bc054ea905ff6349ae43
yweiyen <125293819+yweiyen@users.noreply.github.com>
2024-02-03 07:15:03 +0000

    .

--- GUI/main4.py
+++ GUI/main4.py
@@ -1,11 +1,8 @@
 import streamlit as st
 import pandas as pd
 import requests
-from streamlit_autorefresh import st_autorefresh
 
-st_autorefresh(interval=5000)
-
-st.title("Pico_W_職能發展協會專案")     
+st.title("Pico_W_職能發展協會專案")
 st.header("雞舍:red[溫度]和:blue[光線]狀態")
 st.divider()
 
